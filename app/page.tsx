032ad7a9f2ffb525164d0c30b0e93dae860b15a3
goshhSun <sunw45323@gmail.com>
2026-04-20 16:42:02 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -247,6 +247,7 @@ export default function PlaylistPage() {
           onToggleLoop={toggleLoop}
           onToggleLike={toggleLike}
         />
+      }
       <audio
         ref={audioRef}
         src={currentTrack?.url}
